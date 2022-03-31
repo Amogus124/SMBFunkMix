79676f3f6357d179df56dd747a20860b4611fc67
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-03-31 13:51:29 -0300

    update AndroidControlsMenu.hx

--- source/android/AndroidControlsMenu.hx
+++ source/android/AndroidControlsMenu.hx
@@ -36,7 +36,7 @@ class AndroidControlsMenu extends MusicBeatState
 		config = new Config();
 		curSelected = config.getcontrolmode();
 
-		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menus/base/menuDesat'));
 		bg.scrollFactor.set();
 		bg.color = FlxColor.fromHSB(FlxG.random.int(0, 359), FlxG.random.float(0, 0.8), FlxG.random.float(0.3, 1));
 		add(bg);
