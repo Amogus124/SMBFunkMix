cb78d321027a46288f29b9d9991682e3563584a0
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-03-31 14:07:03 -0300

    update Discord.hx

--- source/meta/data/dependency/Discord.hx
+++ source/meta/data/dependency/Discord.hx
@@ -1,7 +1,9 @@
 package meta.data.dependency;
 
 #if !html5
+#if !android
 import discord_rpc.DiscordRpc;
+#end
 #end
 import lime.app.Application;
 
@@ -15,6 +17,7 @@ class Discord
 	// set up the rich presence initially
 	public static function initializeRPC()
 	{
+                #if !android
 		DiscordRpc.start({
 			clientID: "879525344128925717",
 			onReady: onReady,
@@ -24,17 +27,20 @@ class Discord
 
 		// THANK YOU GEDE
 		Application.current.window.onClose.add(shutdownRPC);
+                #end
 	}
 
 	// from the base game
 	static function onReady()
 	{
+                #if !android
 		DiscordRpc.presence({
 			details: "",
 			state: null,
 			largeImageKey: 'iconog',
 			largeImageText: "Forever Engine"
 		});
+                #end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -51,6 +57,7 @@ class Discord
 
 	public static function changePresence(details:String = '', state:Null<String> = '', ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float)
 	{
+                #if !android
 		var startTimestamp:Float = (hasStartTimestamp) ? Date.now().getTime() : 0;
 
 		if (endTimestamp > 0)
@@ -66,14 +73,17 @@ class Discord
 			startTimestamp: Std.int(startTimestamp / 1000),
 			endTimestamp: Std.int(endTimestamp / 1000)
 		});
+                #end
 
-		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
+		// trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
 
 	public static function shutdownRPC()
 	{
+                #if !android
 		// borrowed from izzy engine -- somewhat, at least
 		DiscordRpc.shutdown();
+                #end
 	}
 	#end
 }
